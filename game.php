de0ef95c745c19e285136ca0429bf6c2bed61c8d
O.M.Gibran <omargibran745@gmail.com>
2024-06-29 15:10:12 +0700

    commit game

--- game.php
+++ game.php
@@ -41,3 +41,14 @@ if (pg_num_rows($result) > 0) {
 // Close database connection
 pg_close($conn);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
