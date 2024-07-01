42d8f320f04469dc6ba3e925106aed32c5aa0fbb
O.M.Gibran <omargibran745@gmail.com>
2024-07-02 00:46:29 +0700

    overall bootstrap theme

--- login.php
+++ login.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if (isset($_SESSION["userid"])) {
+    unset($error); // Unset the $error variable if user is already logged in
+}
+
 // Configuration
 $db_host = 'localhost';
 $db_username = 'postgres';
@@ -40,22 +44,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html>
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
     <title>Login</title>
 </head>
 <body>
-    <h1>Login</h1>
-    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username"><br><br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password"><br><br>
-        <input type="submit" value="Login">
-    </form>
+    <div class="jumbotron vertical-center bg-dark">
+        <div class="container d-flex justify-content-center ">
+        <div class="d-inline-flex  bg-secondary text-light p-3 rounded-4">
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+            <h1>Login</h1>
+            <!-- <div class="col-6"> -->
+            <label for="username" class="form-label" >Username:</label>
+            <input type="text" class="form-control" id="username" name="username"><br><br>
+            <!-- </div> -->
+            <!-- <div class="col-6"> -->
+            <label for="password" class="form-label">Password:</label>
+            <input type="password" class="form-control" id="password" name="password"><br><br>
+            <input type="submit" value="Login">
+            <!-- </div> -->
+            <?php if (isset($error)):?>
+            <p style="color: red;"><?php echo $error;?></p>
+            <?php endif;?>
 
-    <?php if (isset($error)):?>
-        <p style="color: red;"><?php echo $error;?></p>
-    <?php endif;?>
+            <p>Don't have an account? <a href="register.php">Create one now!</a></p>
+        </form>
 
-    <p>Don't have an account? <a href="register.php">Create one now!</a></p>
+        </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
